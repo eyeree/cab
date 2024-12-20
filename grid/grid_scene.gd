28597e1760182d8cb703afcfe5faae70637ac6ae
Mike Deem <mike@eyeree.me>
2024-12-20 13:18:35 -0800

    updated hex grid

--- grid/grid_scene.gd
+++ grid/grid_scene.gd
@@ -15,52 +15,36 @@ const INVALID_HEX_INDEX:Vector2i = Vector2i.MAX
 enum GridSize { SMALL, MEDIUM, LARGE, HUGE }
 
 class GridSizeInfo:
-	var _hex_scale:float
-	var _hex_radius:float
-	var _hex_offset:float
-	var _hex_power:float
-	var _camera_position:Vector3
+	var _hex_max_distance:int
 	var _hex_size:float
-	var _min_x_index:int
-	var _max_x_index:int
-	var _min_y_index:int
-	var _max_y_index:int
+	var _line_size:float
 
 	func _init(
-		hex_scale:float, 
-		hex_radius:float, 
-		hex_offset:float, 
-		hex_power:float,
-		camera_position:Vector3
+		hex_max_distance:int,
+		line_size:float
 	):
-		_hex_scale = hex_scale
-		_hex_radius = hex_radius
-		_hex_offset = hex_offset
-		_hex_power = hex_power
-		_camera_position = camera_position
-		_hex_size = (_hex_radius + _hex_offset) / _hex_scale
+		_line_size = line_size
+		_hex_max_distance = hex_max_distance
+		_hex_size = (1.0 / ((_hex_max_distance*2+1) * sqrt(3))) # (1.0 - _line_size * 2) / ...
 
-	func apply_grid_shader_settings(shader_material:ShaderMaterial) -> void:
-		shader_material.set_shader_parameter('scale', _hex_scale)
-		shader_material.set_shader_parameter('radius', _hex_radius)
-		shader_material.set_shader_parameter('offset', _hex_offset)
-		shader_material.set_shader_parameter('power', _hex_power)
+	func apply_grid_material_settings(grid_material:ShaderMaterial) -> void:
+		grid_material.set_shader_parameter('max_hex_distance', _hex_max_distance)
+		grid_material.set_shader_parameter('line_size', _line_size)
 
-	func apply_camera_settings(camera:Camera3D) -> void:
-		camera.position = _camera_position
-		
 	var hex_size:float:
 		get():
 			return _hex_size
 			
-	func hex_center(hex_index:Vector2i) -> Vector2:
-		var x = _hex_size * (_3_OVER_2 * hex_index.x)
-		var y = _hex_size * (_SQRT_3_OVER_2 * hex_index.x + _SQRT_3 * hex_index.y)
+	func hex_center(index:Vector2i) -> Vector2:
+		# pointy_hex_to_pixel https://www.redblobgames.com/grids/hexagons/#hex-to-pixel-axial
+		var x:float = _hex_size * (sqrt(3) * index.x  +  sqrt(3)/2 * index.y)
+		var y:float = _hex_size * (3.0/2.0 * index.y)
 		return Vector2(x, y)
 		
-	func hex_index(position:Vector2) -> Vector2i:
-		var x = (_2_OVER_3 * position.x) / _hex_size
-		var y = (_MINUS_1_OVER_3 * position.x  + _SQRT_3_OVER_3 * position.y) / _hex_size
+	func hex_index(point:Vector2) -> Vector2i:
+		# pixel_to_pointy_hex https://www.redblobgames.com/grids/hexagons/#pixel-to-hex	
+		var x:float = (sqrt(3)/3 * point.x  -  1.0/3.0 * point.y) / _hex_size
+		var y:float = (2.0/3.0 * point.y) / _hex_size
 		return _axial_round(x, y)
 		
 	func _axial_round(x:float, y:float) -> Vector2i:
@@ -74,10 +58,10 @@ class GridSizeInfo:
 			return Vector2i(x_grid, y_grid + roundi(y + 0.5 * x))
 					
 static var grid_size_info_map: Dictionary[GridSize, GridSizeInfo] = {
-	GridSize.SMALL: GridSizeInfo.new(5.0, 0.45, 0.05, 10.0, Vector3(0.0, 0.007, 0.55)),    # 5
-	GridSize.MEDIUM: GridSizeInfo.new(9.5, 0.45, 0.05, 10.0, Vector3(0.0, 0.007, 0.58)),   # 10
-	GridSize.LARGE: GridSizeInfo.new(13.5, 0.45, 0.05, 10.0, Vector3(0.0, 0.007, 0.61)),   # 15
-	GridSize.HUGE: GridSizeInfo.new(18.0, 0.45, 0.05, 10.0, Vector3(0.0, 0.007, 0.61)),    # 20
+	GridSize.SMALL: GridSizeInfo.new(2, 0.075),
+	GridSize.MEDIUM: GridSizeInfo.new(4, 0.125),
+	GridSize.LARGE: GridSizeInfo.new(7, 0.15),
+	GridSize.HUGE: GridSizeInfo.new(10, 0.175),
 }
 
 @export var grid_size:GridSize = GridSize.SMALL:
@@ -88,16 +72,22 @@ static var grid_size_info_map: Dictionary[GridSize, GridSizeInfo] = {
 
 var _grid_size_info:GridSizeInfo = grid_size_info_map[grid_size]
 
-@onready var grid_shader:ShaderMaterial = %GridPlane.mesh.material
+@onready var grid_material:ShaderMaterial = %GridPlane.mesh.material
 @onready var camera:Camera3D = %Camera
 
 func _ready() -> void:
 	_on_grid_size_set()
 
 func _on_grid_size_set():
+	prints('_on_grid_size_set')
 	if not is_inside_tree(): return
-	_grid_size_info.apply_grid_shader_settings(grid_shader)
-	_grid_size_info.apply_camera_settings(camera)
+	_grid_size_info.apply_grid_material_settings(grid_material)
+	
+func set_selected_index(index:Vector2i) -> void:
+	grid_material.set_shader_parameter('selected_index', index)
+	
+func clear_selected_index(index:Vector2i) -> void:
+	grid_material.set_shader_parameter('selected_index', Vector2i(1000, 1000))
 
 func _mouse_position_to_grid_position(mouse_position:Vector2) -> Vector2:
 	var drop_plane:Plane = Plane(Vector3(0, 0, 1), 0)
@@ -111,6 +101,7 @@ func _input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion:
 		var grid_position:Vector2 = _mouse_position_to_grid_position(event.position)
 		var hex_index:Vector2i = _grid_size_info.hex_index(grid_position)
+		set_selected_index(hex_index)
 		var hex_center:Vector2 = _grid_size_info.hex_center(hex_index)
 		prints('_input', event.position, '->', hex_index, hex_center)
 	elif event is InputEventKey:
